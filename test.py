bcf2763e3d9779f69e50e5f4f46c26c9ac9a6a39
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-01-27 18:34:42 +0900

    getchをコネコネしてた雑多なやつ

--- test.py
+++ test.py
@@ -1,16 +1,38 @@
 #%%
 import i2v
 from PIL import Image
+from pathlib import Path
+from pprint import pprint
+from tqdm import tqdm
+import pickle
 #%%
+import i2v
+from PIL import Image
 
+illust2vec = i2v.make_i2v_with_chainer(
+    "illust2vec_tag_ver200.caffemodel", "tag_list.json")
+# %%
+root = Path("/data/natsuki/getchu_s5")
+assert root.is_dir()
+
+BatchSize = 32
+filelist = list(root.glob("*"))
+outs = list()
+for i in tqdm(range((len(filelist)-1)//BatchSize+1)):
+    imgs = [Image.open(fname) for fname in filelist[i*BatchSize:(i+1)*BatchSize]]
+    outs += illust2vec.estimate_plausible_tags(imgs, threshold=0)
+# %%
+for i in tqdm(range(len(outs))):
+    for k in outs[i]:
+        outs[i][k] = dict(outs[i][k])
+# %%
+with open((root/f"/data/natsuki/getchu_s5_i2v.pkl"), "wb") as f:
+    pickle.dump(dict(zip(list(map(str, filelist)), outs)), f, protocol=4)
+# %%
 illust2vec = i2v.PytorchI2V(
             "illust2vec_tag_ver200.pth", "tag_list.json")
-#%%
-
 img = Image.open("images/miku.jpg")
 x = illust2vec._estimate([img])
-for i in x[0]:
-    print(float(i))
-
-
+print(x.shape)
+# %%
 # %%
